06ab6811b8cc98a76d40aa780c432d516d96ee69
Joshna Pachigolla <joshna.pachigolla@tigeranalytics.local>
2022-05-19 12:39:00 +0530

    first commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,37 @@
-export default function Profile(){
-    return(
-        <>
-        <h2>This is Profile page - Functional Component</h2>
-        </>
-    )
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function Profile() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="280"
+          image="https://images.unsplash.com/photo-1544005313-94ddf0286df2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Štefan Štefančík
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           Name: Štefan Štefančík<br/>
+           Email Id: stefan@gmail.com<br/>
+           Age: 22<br/>
+           Interests: Nature Images,Mountain Images , Pictures, Travel Images, Fashion.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Edit Profile
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
