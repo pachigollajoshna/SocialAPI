6075f953d724dcb0d9f2d276c21571c050c9ac05
Joshna Pachigolla <joshna.pachigolla@tigeranalytics.local>
2022-05-19 11:15:50 +0530

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 class Login extends Component {
   render() {
     return (
       <>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+    <div>
+      <TextField id="outlined-basic" label="Enter EmailID" variant="outlined" /><br/><br/>
+      <TextField id="outlined-basic" label="Enter Password" variant="outlined" /><br/><br/>
+      <Button variant="contained" href="#">Login</Button>
+    </div>
+    </Box>
         
       </>
     )
